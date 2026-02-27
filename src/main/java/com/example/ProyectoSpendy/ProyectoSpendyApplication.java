affde0ebe3c6ca0049a9402f5f8512e69db4cd9c
Juan Gonzalo Alvarez <juangonzalo22@gmail.com>
2026-02-27 17:19:13 -0500

    feat: inicialización del proyecto Spring Boot ProyectoSpendy

---
+++ src/main/java/com/example/ProyectoSpendy/ProyectoSpendyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoSpendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpendyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpendyApplication.class, args);
+	}
+
+}
